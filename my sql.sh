801470d88946d4c0949459f793b83e85633d08ca
vamshi <panja.vamshi2023@gmail.com>
2024-10-14 22:23:15 -0500

    variables

---
+++ my sql.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/ecpense"
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+TIMESTAMP=$(date +%y-%m-%d-%-H-%M-%-S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R=" \e[31m"
+G=" \e[32m"
+G=" \e[30m"
+Y=" \e[30m"
+
+CHECK_ROOT (){
+    if [ $USERID -ne 0 ]
+    then 
+       echo  -e "$R please run this script with root priveleges $N" | tee -a $LOG_FILE
+       exit 1
+    fi 
+}
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+       echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
+       exit 1
+  else 
+      echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE
+ fi
+}
+
+    echo "script started executing at: $(date)" | tee -a $LOG_FILE
+    
+    CHECK_ROOT
+
+    dnf install mysql-server -y
+    VALIDATE $? "installing mySQL server"
+
+    systemctl enable mysqld
+    VALIDATE $? "Enabled MYSQL server"
+
+    systemctl start mysqld
+    VALIDATE $? "Enabled MYSQL server"
+
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "setting up root password"
+    
+
+    
